33630ad34bc48ebd8286ba6df78a81a8ec2d6da4
Adilah Syamim <adyla.dyla.atm@gmail.com>
2026-05-20 14:15:27 +0800

    Changed some process for AIR-COOLED

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ import {
 } from "./processRuns.js";
 
 // Added app versioning for checking purposes
-const APP_VERSION = "2026-05-18-04";
+const APP_VERSION = "2026-05-20-01";
 let updateAvailable = false;
 let latestVersion = APP_VERSION;
 
